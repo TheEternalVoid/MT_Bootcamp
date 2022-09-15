1459bc28cb5efb6d06f3fd759b639a78602b09dd
Larry Holland <larry@wsw.com>
2022-09-15 18:59:34 -0400

    Added new tables for class, guest, status

--- HWASSIGNMENT1.sql
+++ HWASSIGNMENT1.sql
@@ -1,10 +1,13 @@
-DROP TABLE IF EXISTS Taverns;
+DROP TABLE IF EXISTS Statuses;
+DROP TABLE IF EXISTS Classes;
 DROP TABLE IF EXISTS Supplies;
-DROP TABLE IF EXISTS TavernSupplies;
 DROP TABLE IF EXISTS OurServices;
+DROP TABLE IF EXISTS Taverns;
+DROP TABLE IF EXISTS TavernSupplies;
 DROP TABLE IF EXISTS StatusTypes;
 DROP TABLE IF EXISTS TavernServices;
 DROP TABLE IF EXISTS ServiceSales;
+DROP TABLE IF EXISTS Guests;
 
 CREATE TABLE Taverns (
 	Id INTEGER NOT NULL IDENTITY(1,1) PRIMARY KEY,
@@ -55,6 +58,29 @@ CREATE TABLE ServiceSales (
 	CostOfService INTEGER NOT NULL,
 	DatePurchased DATE NOT NULL,
 	AmountPurchased INTEGER NOT NULL
+);
+
+CREATE Table Statuses (
+	Id Integer NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	StatusName VARCHAR(50) NOT NULL
+);
+
+CREATE Table Classes (
+	Id INTEGER NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	ClassName VARCHAR(50),
+);
+
+
+CREATE TABLE Guests (
+	Id INTEGER NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	GuestName VARCHAR(50) NOT NULL,
+	NotesAboutGuest VARCHAR(100),
+	GuestBday DATE NOT NULL,
+	GuestStatusId INTEGER NOT NULL,
+	FOREIGN KEY (GuestStatusId) REFERENCES Statuses(Id),
+	GuestClassId INTEGER NOT NULL,
+	FOREIGN KEY (GuestClassId) REFERENCES Classes(Id),
+	GuestLevel INTEGER NOT NULL
 );
 
 INSERT INTO Taverns (TavernName)
@@ -112,3 +138,27 @@ VALUES
 	(1, 'Tyler Hill', 10, '1958-01-31', 1),
 	(4, 'Bob Jim', 5, '1959-01-31', 1),
 	(5, 'Tyler Hill', 8, '1958-01-31', 1);
+
+INSERT INTO Statuses (StatusName)
+VALUES
+	('sick'),
+	('fine'),
+	('hangry'),
+	('raging'),
+	('placid');
+
+INSERT INTO Classes (ClassName)
+VALUES
+	('Fighter'),
+	('Paladin'),
+	('Mage'),
+	('Rogue'),
+	('Druid');
+
+INSERT INTO Guests (GuestName, NotesAboutGuest, GuestBday, GuestStatusId, GuestClassId, GuestLevel)
+VALUES
+	('Tyler Hill', 'Dead inside', '1958-01-31', 5, 1, 1),
+	('Jerome Hill', 'Also Dead inside', '1959-01-31', 3, 2, 10),
+	('Larry Holland', 'Super Strong', '1997-01-31', 2, 1, 20),
+	('Kayle Gross', 'An actual druid', '1969-02-14', 4, 5, 7),
+	('Olley Dolley', 'Super Sweet', '1919-12-25', 1, 3, 19);
